886b1b56ef01970c4c9f26f76030397f5f92ef62
SamuelGK <89768420+SamuelGK@users.noreply.github.com>
2021-09-08 14:07:26 +0200

    Add project files.

---
+++ Assignment/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace Assignemnt
+{
+    class Program
+    {
+        static object Main(string[] args)
+        {
+            do
+            {
+
+                double num0 = 0;
+
+                double num1 = 0;
+                double result = 0;
+
+                Console.Clear();
+                Console.WriteLine("--- Menu ---");
+                Console.WriteLine("Calculator Program");
+
+                Console.WriteLine("Enter a number 0: ");
+                num0 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter a number 1: ");
+                num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Please enter a command:");
+                Console.WriteLine("* : Multiply ");
+                Console.WriteLine("+ : Add");
+                Console.WriteLine("- : Substract");
+                Console.WriteLine("/ : Divide");
+
+
+                switch (Console.ReadLine())
+                {
+
+                    case "*":
+                        result = num0 * num1;
+                        Console.WriteLine($"Your answer: {num0} * {num1} = " + result);
+                        break;
+
+                    case "+":
+                        result = num0 + num1;
+                        Console.WriteLine($"Your answer: {num0} + {num1} = " + result);
+                        break;
+
+                    case "-":
+                        result = num0 - num1;
+                        Console.WriteLine($"Your answer: {num0} - {num1} = " + result);
+                        break;
+
+                    case "/":
+                        result = num0 / num1;
+                        Console.WriteLine($"Your answer: {num0} / {num1} = " + result);
+                        break;
+
+                    default:
+                        Console.WriteLine("Unvalid command");
+                        break; //end of main
+                }
+                Console.WriteLine("Keep going? (O = ok, N = no)");
+
+
+
+            } while (Console.ReadLine().ToUpper() == "Y");
+            Console.WriteLine("Goodbye");
+            Console.ReadKey();
+            // end of program
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+
+    }
+}
